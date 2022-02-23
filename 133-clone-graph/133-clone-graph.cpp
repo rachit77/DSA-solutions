c74c192fd5ad7676f153d189d6e75ca50d3fc38b
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-02-23 02:17:06 -0800

    Time: 4 ms (93.09%), Space: 8.9 MB (45.65%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+
+
+class Solution {
+public:
+    void dfs(Node* copy, Node* node, vector<Node*> &vis) 
+    {
+        vis[node->val]= copy;
+        
+        for(auto x: node->neighbors) {
+            if(vis[x->val] ==NULL)  //not visited
+            {
+               Node* newNode= new Node(x->val);
+                (copy->neighbors).push_back(newNode);
+                dfs(newNode,x,vis);
+            }
+            else {
+                (copy->neighbors).push_back(vis[x->val]);
+                
+            }
+        }
+    }
+    
+    
+    Node* cloneGraph(Node* node) {
+        
+        if(node == NULL)
+            return NULL;
+        vector<Node*> vis(101,NULL);
+        Node* copy= new Node(node->val);
+        
+        
+        dfs(copy,node,vis);
+        return copy;
+        
+    }
+};

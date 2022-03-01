829693077fe49de0af04571a727748f9ae428c98
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-01 01:03:13 -0800

    Time: 2 ms (37.05%), Space: 6.8 MB (61.88%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -11,7 +11,7 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(i == n-1 || nums[i]+1 != nums[i+1])
+            if(i == n-1 || nums[i+1]!=nums[i]+1  )
             {
                 if(st!=nums[i])  
                     s.push_back(to_string(st) +"->"+to_string(nums[i]));

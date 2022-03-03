3fe29d2c657224dae460f8ad5549f575584330d2
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-03 08:08:24 -0800

    Time: 1696 ms (27.69%), Space: 13.3 MB (66.20%) - LeetHub

--- 313-super-ugly-number/313-super-ugly-number.cpp
+++ 313-super-ugly-number/313-super-ugly-number.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        
-//        int len=primes.size();
-        
-//         vector<int> v(len,0);  
-        
-//         vector<int> dp(n,0);
-//         dp[0]=1;
-        
+//          vector<int> dp(n+1,0); int k=primes.size();
+//          dp[0]=1;
+//         vector<int> pointer(k,0);   
 //         for(int i=1;i<n;i++)
 //         {
-//            int temp= INT_MAX;
-//             for(int j=0;j<len;j++)
+//            int mn= INT_MAX;
+//             for(int j=0;j<k;j++)
 //             {
-//                 temp=min(temp, primes[j]* dp[v[j]]);
+//                 mn=min(primes[j]* dp[pointer[j]], mn);
 //             }
             
-//             dp[i]=temp;
-            
-//             for(int j=0;j<len;j++)
+//             dp[i]=mn;
+//             for(int j=0;j<k;j++)
 //             {
-//                 if(dp[i]==   primes[j]* dp[v[j]] ) 
-//                 {
-//                     v[j]++;
-                    
-//                 }
-//             }
-//             // cout<<i<<" "<<dp[i]<<endl;
+//                 if(primes[j]* dp[pointer[j]] ==mn) 
+//                     pointer[j]++;   
+//             }  
 //         }
         
 //         return dp[n-1];
         
         
-        vector<int> dp(n+1,0); int k=primes.size();
-   // memset(dp,0,sizeof(dp));
+        vector<int> dp(n,0); int k=primes.size();
+   
     dp[0]=1;
     vector<int> pointer(k,0);
-    //memset(pointer,0,sizeof(pointer));
+    
     for(int i=1; i<n; i++){
         int mn=INT_MAX;
         for(int j=0; j<k; j++){

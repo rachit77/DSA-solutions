dee60e6f9b9581c2c7f30174b40787fe9b5ad84f
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-06 09:58:48 -0800

    Time: 32 ms (26.79%), Space: 14.6 MB (23.65%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    unordered_map<string,int> dp;
+    
+    int solve(string s,vector<string> &b ) {
+        
+        int siz=s.length();
+        if(siz==0) return 1;
+        
+        if(dp[s]!=0)  // string already present in dp
+            return dp[s];
+        
+        for(int i=1;i<=siz;i++)  // make the string to be searched
+        {
+            string ss=s.substr(0,i);
+            int f=0;
+            
+            for(int j=0;j<b.size();j++)
+            {
+                if(ss.compare(b[j])==0) // we got the string in dict
+                {
+                    f=1;
+                    break;
+                }
+                
+            }
+            
+                if(f==1 && (solve(s.substr(i,siz-i),b)==1)){
+                   return dp[s]=1;
+                
+            } 
+        }
+        return dp[s]=-1;
+        
+    }
+    
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        int x= solve(s,wordDict);
+        if(x==1)
+            return 1;
+        else
+            return 0;
+        
+    }
+};

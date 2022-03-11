077203d69550b11b9665f8bea291dd98d3d16a12
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-11 13:30:36 -0800

    Time: 12 ms (42.98%), Space: 13.8 MB (73.28%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n=nums.size();
+
+// correct solution but TLE , O(n^2)       
+        
+//         int prd=INT_MIN;
+        
+//         for(int i=0;i<n;i++) {
+//             int prev=1;
+//             for(int j=i;j<n;j++) 
+//             {
+//                 prev=prev*nums[j];
+                
+//                 prd=max(prd,prev);
+//             }
+//         }
+        
+//         return prd;
+        
+//2nd solution
+int ma=nums[0],mi=nums[0],ans=nums[0];
+        
+        for(int i=1;i<n;i++) {
+            
+            if(nums[i]<0)
+            {
+                int temp=ma;
+                ma=mi;
+                mi=temp;
+            }
+            
+            ma=max(nums[i],ma*nums[i]);
+            
+            mi= min(nums[i],mi*nums[i]);
+            
+            ans=max(ans,ma);
+            
+        }
+        
+        return ans;
+        
+    }
+};

9f29903919a9a95f585ba54550cd9f73243c8b29
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-12 10:03:09 -0800

    Time: 0 ms (100.00%), Space: 9.1 MB (78.29%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
-    int subset(vector<int>nums,int sum){
-        int n=nums.size();
-        int dp[n+1][sum+1];
-        for(int i=0;i<=n;i++)
-            dp[i][0]=1;
-        for(int j=1;j<=sum;j++)
-            dp[0][j]=0;
-       
-        
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                dp[i][j]=dp[i-1][j];
-                if(nums[i-1]<=j)
-                    dp[i][j]=dp[i][j]+dp[i-1][j-nums[i-1]];
-            }
-        }
-        return dp[n][sum];        
-    }
+   
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int i,s=0;
@@ -25,71 +8,30 @@ public:
             s+=nums[i];
         
         
-//         target=abs(target);
+        target=abs(target);
         
        int  new_target=(s+target);
         new_target=new_target/2;
             
-              if((s+target)%2==1 || s<new_target) return 0;
-        
-         
-        
-       new_target = abs(new_target);
-        
-        
-        int dp[n+1][new_target+1];
-        
-//         // fill the dp with some intial values
+       if((s+target)%2==1 || s<new_target) return 0;
+     
+       //new_target = abs(new_target);
+       int dp[n+1][new_target+1];
        
+         for(i=0;i<new_target+1;i++) dp[0][i]=0;
         
-         for(i=0;i<new_target+1;i++)
-            dp[0][i]=0;
-        
-        for(i=0;i<n+1;i++)
-            dp[i][0]=1;
-        
-       
-        
+        for(i=0;i<n+1;i++) dp[i][0]=1;
+ 
 
         for(i=1;i<=n;i++) {
             for(int j=0;j<=new_target;j++) {
-                
-
-                    dp[i][j]= dp[i-1][j];
+                 dp[i][j]= dp[i-1][j];
                  if(nums[i-1]<=j)
                     dp[i][j]=dp[i][j] + dp[i-1][j-nums[i-1]];
-                    
                    }
         }  
-
-                
-               
-                
-        //          for(int i=1;i<=n;i++){
-        //     for(int j=0;j<=new_target;j++){
-        //         dp[i][j]=dp[i-1][j];
-        //         if(nums[i-1]<=j)
-        //             dp[i][j]=dp[i][j]+dp[i-1][j-nums[i-1]];
-        //     }
-        // }
-                
-
-                
-       
         
-        
-        
-        int ans= dp[n][new_target];
-        
-        return ans;
-        
-        
-//         int s=0;
-//         s=accumulate(nums.begin(),nums.end(),s);
-//         int p=(target+s)/2;
-//         if(s < target || (s + target) % 2 != 0)
-//             return 0;
-//         return subset(nums,abs(p));   
+        return dp[n][new_target];  
         
     }
 };

ac777cf806a2a2ecbb5923ee3432cdeca3048b16
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-12 10:00:42 -0800

    Time: 4 ms (98.61%), Space: 9 MB (78.29%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -7,6 +7,8 @@ public:
             dp[i][0]=1;
         for(int j=1;j<=sum;j++)
             dp[0][j]=0;
+       
+        
         for(int i=1;i<=n;i++){
             for(int j=0;j<=sum;j++){
                 dp[i][j]=dp[i-1][j];
@@ -17,72 +19,77 @@ public:
         return dp[n][sum];        
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-//         int n=nums.size();
-//         int i,s=0;
-//         for(i=0;i<n;i++)
-//             s+=nums[i];
+        int n=nums.size();
+        int i,s=0;
+        for(i=0;i<n;i++)
+            s+=nums[i];
         
         
 //         target=abs(target);
         
-//       int  new_target=(s+target);
-        
+       int  new_target=(s+target);
+        new_target=new_target/2;
             
-//              if(new_target%2==1 || s<new_target) return 0;
+              if((s+target)%2==1 || s<new_target) return 0;
         
-//         new_target=new_target/2;
+         
         
-//         //new_target = abs(new_target);
+       new_target = abs(new_target);
         
         
-//         int dp[n+1][new_target+1];
+        int dp[n+1][new_target+1];
         
 //         // fill the dp with some intial values
        
         
-//          for(i=0;i<new_target+1;i++)
-//             dp[0][i]=0;
+         for(i=0;i<new_target+1;i++)
+            dp[0][i]=0;
         
-//         for(i=0;i<n+1;i++)
-//             dp[i][0]=1;
+        for(i=0;i<n+1;i++)
+            dp[i][0]=1;
         
        
         
-//         //cout<<dp[0][0]<<"VF"<<dp[0][1]<<endl;
-        
-//         // fill the complete
-//         for(i=1;i<n+1;i++) {
-//             for(int j=1;j<new_target+1;j++) {
+
+        for(i=1;i<=n;i++) {
+            for(int j=0;j<=new_target;j++) {
                 
-//                 if(nums[i-1] > j)
-//                     dp[i][j]= dp[i-1][j];
-                
-//                 else {
-                
-//                 //if(nums[i-1]<=j)
-//                     dp[i][j]=dp[i][j] + dp[i-1][j-nums[i-1]];
+
+                    dp[i][j]= dp[i-1][j];
+                 if(nums[i-1]<=j)
+                    dp[i][j]=dp[i][j] + dp[i-1][j-nums[i-1]];
                     
-//             //cout<<"frf"<<dp[i-1][j]<<" "<<dp[i-1][j-nums[i-1]]<<endl;
-//                 }
+                   }
+        }  
+
                 
-//                 //cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
+               
                 
-//             }
-//         }
+        //          for(int i=1;i<=n;i++){
+        //     for(int j=0;j<=new_target;j++){
+        //         dp[i][j]=dp[i-1][j];
+        //         if(nums[i-1]<=j)
+        //             dp[i][j]=dp[i][j]+dp[i-1][j-nums[i-1]];
+        //     }
+        // }
+                
+
+                
+       
         
         
         
-//         int ans= dp[n][new_target];
+        int ans= dp[n][new_target];
         
-//         return ans;
+        return ans;
         
         
-        int s=0;
-        s=accumulate(nums.begin(),nums.end(),s);
-        int p=(target+s)/2;
-        if(s < target || (s + target) % 2 != 0)
-            return 0;
-        return subset(nums,abs(p));   
+//         int s=0;
+//         s=accumulate(nums.begin(),nums.end(),s);
+//         int p=(target+s)/2;
+//         if(s < target || (s + target) % 2 != 0)
+//             return 0;
+//         return subset(nums,abs(p));   
         
     }
 };

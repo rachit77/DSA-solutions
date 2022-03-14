2765bb8c1d93ff304157958c8a4d8f9ace179669
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-14 12:49:09 -0700

    Time: 12 ms (31.65%), Space: 11.1 MB (11.04%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -2,91 +2,133 @@ class Solution {
 public:
     string simplifyPath(string path) {
         
-        stack<string> st;
-        int i=0;
+//         stack<string> st;
+//         int i=0;
         
-        //add slash at begining and end
-        path='/'+path+'/';
+//         //add slash at begining and end
+//         path='/'+path+'/';
         
-        int n=path.length();
-        while(i<n) {
+//         int n=path.length();
+        
+//         //add all elements in stack
+//         while(i<n) {
             
-            if(path[i]=='/')
-            {
-                i++;
-                string temp;
-                while(i<n and path[i]!='/')
-                {
-                    temp=temp+path[i];
-                    i++;
-                }
-                //cout<<i<<" "<<temp<<endl;
-                if(!temp.empty()){
-                  st.push(temp);
-                   //cout<<"tt"<<temp<<endl; 
-                } 
+//             if(path[i]=='/')
+//             {
+//                 i++;
+//                 string temp;
+//                 while(i<n and path[i]!='/')
+//                 {
+//                     temp=temp+path[i];
+//                     i++;
+//                 }
+//                 if(!temp.empty()){
+//                   st.push(temp);
+//                 } 
                 
-            }
-            else i++;
-        }
+//             }
+//             else i++;
+//         }
         
-        //cout<<"while over "<<endl;
         
-        stack<string> k= st;
-        //cout<<"vjnv"<<st.empty()<<endl;
-        // while(!k.empty())
-        // {
-        //     cout<<k.top()<<endl;
-        //         k.pop();
-        // }
+//         string ans;
         
-        //process the stack
+//     while(!st.empty()) {
+//             string tem=st.top();
+//             st.pop();
         
-        string ans;
-        
-    while(!st.empty()) {
-            string tem=st.top();
-            st.pop();
-        
-        // single period
-        if(tem==".")
-            continue;
-        else if(tem=="..") // double period
-        {
-            int back=1;
+//         // single period
+//         if(tem==".")
+//             continue;
+//         else if(tem=="..") // double period
+//         {
+//             int back=1;
             
-            while(back!=0 and !st.empty())
-            {
-                    string ss=st.top();
-                    st.pop();
+//             while(back!=0 and !st.empty())
+//             {
+//                     string ss=st.top();
+//                     st.pop();
                 
-                if(ss==".")
-                continue;
-                else if(ss=="..")
-                    back++;
-                else {
-                    back--;
-                    //ans=ss+'/'+ans;
-                }
-            }
+//                 if(ss==".")
+//                 continue;
+//                 else if(ss=="..")
+//                     back++;
+//                 else {
+//                     back--;
+//                 }
+//             }
             
-        }
-        else {
-            ans=tem+'/'+ans;
-        }
-    }
+//         }
+//         else {
+//             ans=tem+'/'+ans;
+//         }
+//     }
         
         
     
-        if(!ans.empty() && ans[0]!='/')
-            ans='/'+ans;
+//         if(!ans.empty() && ans[0]!='/')
+//             ans='/'+ans;
         
-        if(ans.empty()) ans='/';
+//         if(ans.empty()) ans='/';
         
-        int len=ans.length();
+//         int len=ans.length();
         
-        if(len>1 && ans[len-1]=='/')
-            ans=ans.substr(0,len-1);
+//         if(len>1 && ans[len-1]=='/')
+//             ans=ans.substr(0,len-1);
+        
+//         return ans;
+        
+        
+//2nd method cleaner code
+        
+        stack<string> st;
+        int i=1,n;
+        
+        path=path+'/';
+        string temp;
+        
+        n=path.length();
+        
+        while(i<n)
+        {
+          if(path[i]=='/')
+          {
+             if(temp=="" || temp==".")
+             {
+               //   i++;
+               // continue;  
+             }
+                
+              else if(temp=="..")
+              {
+                  if(!st.empty())
+                      st.pop();
+              }
+              else
+              {
+                  st.push(temp);
+              }
+              temp="";
+          }
+            else
+                //temp.push_back(path[i]);
+                temp=temp+path[i];
+            
+            i++;
+              
+        }
+        
+        
+        //process the stack
+        string ans;
+        while(!st.empty()) {
+            ans="/"+st.top()+ans;
+            st.pop();
+        }
+        
+        //check if string is empty
+        if(ans.empty())
+            ans="/";
         
         return ans;
         

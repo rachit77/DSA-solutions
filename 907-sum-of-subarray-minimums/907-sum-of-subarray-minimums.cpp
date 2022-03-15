41ada9256ac4f1d4873e1465d32962a307d7d94f
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-15 08:55:42 -0700

    Time: 218 ms (9.22%), Space: 42.9 MB (57.43%) - LeetHub

---
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        
+        int n=arr.size();
+        
+        //next smaller elemnet and prev smaller element
+        vector<int> nex(n,0);
+        vector<int> prev(n);
+        
+        stack<int> st;
+        
+        int i;
+        
+        //next smaller element
+        for(i=0;i<n;i++) {
+            
+            while(!st.empty() && arr[i]<=arr[st.top()])
+            {
+                nex[st.top()]=i-1;
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        
+        while(!st.empty()) {
+            nex[st.top()]=n-1;
+            st.pop();
+        }
+        
+        
+       
+        
+        //test print nex vector
+        
+//         for(i=0;i<n;i++)
+//         {
+//             cout<<nex[i]<<endl;
+//         }
+        
+        
+        
+        
+        //previous smaller element
+        for(i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && arr[i]<arr[st.top()]) {
+               
+                prev[st.top()]=i+1;
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        
+        while(!st.empty())
+        {
+            prev[st.top()]=0;
+            st.pop();
+        }
+        
+        
+        //test print nex vector
+        // cout<<"------"<<endl;
+        // for(i=0;i<n;i++)
+        // {
+        //     cout<<prev[i]<<endl;
+        // }
+        
+        //now cal the sum of min in those sub arrays
+        int ans=0;
+        long long mod = 1e9 + 7;
+        
+        for(int i=0;i<n;i++){
+            long long left=i-prev[i]+1;
+            // if(left<0)
+            // {
+            //     cout<<i<<" "<<left<<endl;
+            // }
+            long long right=nex[i]-i+1;
+            // if(right<0)
+            // {
+            //     cout<<i<<" "<<right<<endl;
+            // }
+            
+            ans+= (arr[i]* ( (left*right)%mod) ) %mod;
+            ans=ans%mod;
+        //     if(ans<0)
+        //         cout<<i<<"v "<<ans<<endl;
+         }
+        
+        return ans;
+        
+        
+    }
+};

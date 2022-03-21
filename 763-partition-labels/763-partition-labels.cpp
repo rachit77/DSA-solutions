ff4b0be1f59d648fad6806e5edb1657983d50fe2
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-03-21 08:11:53 -0700

    Time: 4 ms (72.57%), Space: 6.5 MB (87.54%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -4,37 +4,23 @@ public:
         
         vector<int> ans;
         
-        vector<pair<int,int>> rec(26,{-1,-1});
+        vector<int> rec(26,-1);
         int i,n;
         n=s.length();
         
-        for(i=0;i<n;i++) {
-            int temp=s[i]-'a';
-            
-        rec[temp].first=min(i,rec[temp].first);
-            
-        rec[temp].second=max(i,rec[temp].second);
-        }
-        
-        
+        for(i=0;i<n;i++) 
+          rec[s[i]-'a']=max(i,rec[s[i]-'a']);
+         
         int cur_len=0,br=-1;
         
-        //cout<<rec[0].second<<endl;
         
         for(i=0;i<n;i++) {
             
             if(br==-1)
-            {
-                br=rec[s[i]-'a'].second;
+                br=rec[s[i]-'a'];
                 
-            }
-            else {
-                if(i<br)
-                {
-                    br=max(br,rec[s[i]-'a'].second);
-                    
-                }
-            }
+            else 
+                br=max(br,rec[s[i]-'a']);
                 
               cur_len++;  
                 if(br==i)
@@ -44,18 +30,11 @@ public:
                     cur_len=0;
                     br=-1;
                 }
-            //}
-            
-            //cout<<i<<" "<<br<<" "<<cur_len<<endl;
-            
-            
-            
         }
         
         if(cur_len)
             ans.push_back(cur_len);
-        
-        
+    
         return ans;
         
     }

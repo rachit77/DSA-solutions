533560ce02061f0cb53db1c961cfe22e2c02816c
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-04-02 10:48:07 -0700

    Time: 0 ms (100.00%), Space: 9.6 MB (53.09%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+     // approach:
+     // find the row in which the target lies
+    // use binary sort in the row to fin the target
+    // time complexity: m * log(n)
+        
+        int m,n,i;
+        m=matrix.size();   //rows
+        n=matrix[0].size();  // columns
+        
+        int row=-1;
+        
+        for(i=0;i<m;i++)
+        {
+            if(target<= matrix[i][n-1])
+            {
+                row=i;
+                break;
+            }
+        }
+        
+        if(row==-1)
+            return false;
+        
+        int l=0,h=n-1,mid;
+        
+        while(l<=h)
+        {
+            mid=l+(h-l)/2;
+            
+            if(matrix[row][mid]==target)
+                return true;
+            
+            if(matrix[row][mid] < target)
+            {
+                l=mid+1;
+            }
+            else {
+                h=mid-1;
+            }
+        }
+        
+        
+        return false;
+        
+    }
+};

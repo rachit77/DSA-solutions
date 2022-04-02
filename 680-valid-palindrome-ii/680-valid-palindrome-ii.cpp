cf83b85ee7e55e1def2d01f785003f5cc5222c96
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-04-02 00:42:07 -0700

    Time: 84 ms (25.44%), Space: 23.9 MB (39.29%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool check(int l, int h, string s)
+    {
+        while(l<h)
+        {
+            if(s[l]!=s[h])
+                return false;
+            
+            l++; h--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        
+        int l=0,h=s.length()-1;
+
+        while(l<h) {
+            if(s[l]!=s[h])
+                return check(l+1,h,s) || check(l,h-1,s);
+            
+            l++;  h--;   
+        }        
+        return true;
+        
+    }
+};

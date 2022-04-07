dac709f2f2fa7641892e10a0643b741545ba06e1
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-04-08 01:19:58 +0530

    Time: 4 ms (37.25%), Space: 7.5 MB (79.44%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        //make max heap
+        make_heap(stones.begin(), stones.end());
+        
+        while(stones.size() >1)
+        {
+            int x= stones.front();
+            pop_heap(stones.begin(),stones.end());
+            stones.pop_back();
+            
+            int y = stones.front();
+            pop_heap(stones.begin(),stones.end());
+            stones.pop_back();
+
+            
+            if(x==y)
+                continue;
+            
+            stones.push_back(x-y);
+            push_heap(stones.begin(), stones.end());
+            
+        }
+        
+        return stones.size()? stones.front():0;
+    }
+};

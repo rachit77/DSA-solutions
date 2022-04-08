fd8d9233218d05a7e26540896de15c8312e521d4
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-04-08 12:13:43 -0700

    Time: 4 ms (37.25%), Space: 7.6 MB (34.36%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
+    int lastStoneWeight(vector<int>& v) {
         //make max heap
-        make_heap(stones.begin(), stones.end());
+//         make_heap(stones.begin(), stones.end());
         
-        while(stones.size() >1)
+//         while(stones.size() >1)
+//         {
+//             int x= stones.front();
+//             pop_heap(stones.begin(),stones.end());
+//             stones.pop_back();
+            
+//             int y = stones.front();
+//             pop_heap(stones.begin(),stones.end());
+//             stones.pop_back();
+            
+//             if(x==y)
+//                 continue;
+            
+//             stones.push_back(x-y);
+//             push_heap(stones.begin(), stones.end());
+            
+//         }        
+//         return stones.size()? stones.front():0;
+        
+        
+        priority_queue<int> pq(v.begin(),v.end());
+        
+        while(true)
         {
-            int x= stones.front();
-            pop_heap(stones.begin(),stones.end());
-            stones.pop_back();
-            
-            int y = stones.front();
-            pop_heap(stones.begin(),stones.end());
-            stones.pop_back();
-            
-            if(x==y)
-                continue;
-            
-            stones.push_back(x-y);
-            push_heap(stones.begin(), stones.end());
-            
-        }        
-        return stones.size()? stones.front():0;
+            if(pq.size() ==0)   return 0;
+            if(pq.size() ==1)   return pq.top();
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(a!=b)    pq.push(a-b);
+        }
     }
 };

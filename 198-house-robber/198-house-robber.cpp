cc654110a6d1f31483abed86e5a2c17a5b98466a
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-08 08:13:22 -0700

    Time: 0 ms (100.00%), Space: 7.7 MB (76.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -15,27 +15,46 @@ class Solution {
  
     
     // 2. Memoization approach
-    private:
-    int maxsum(vector<int> &nums, int id, vector<int> &dp)
-    {
-        if(id>=nums.size()) return 0;
+//     private:
+//     int maxsum(vector<int> &nums, int id, vector<int> &dp)
+//     {
+//         if(id>=nums.size()) return 0;
         
-        if(dp[id]!=-1)
-            return dp[id];
+//         if(dp[id]!=-1)
+//             return dp[id];
         
-        return dp[id]=max(nums[id]+maxsum(nums,id+2,dp), maxsum(nums,id+1,dp));
-    }
-    public:
-    int rob(vector<int>& nums) {
-        
-        vector<int> dp(nums.size(),-1);
-        
-        return maxsum(nums,0,dp);
-        
-    }
-
+//         return dp[id]=max(nums[id]+maxsum(nums,id+2,dp), maxsum(nums,id+1,dp));
+//     }
 //     public:
 //     int rob(vector<int>& nums) {
         
+//         vector<int> dp(nums.size(),-1);
+        
+//         return maxsum(nums,0,dp);
+        
 //     }
+
+    
+    // 3. Top Down Aproach
+    public:
+    int rob(vector<int>& nums) {
+        
+        vector<int> dp(nums.size(),0);
+        
+        int n=nums.size();
+        
+        if(n>=1)
+        dp[0]=nums[0];
+        
+        if(n>=2)
+        dp[1]= max(nums[0],nums[1]);
+        
+        for(int i=2;i<nums.size();i++)
+        {
+            dp[i]= max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        
+        return dp[nums.size()-1];
+        
+    }
 };

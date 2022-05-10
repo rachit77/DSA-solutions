641591169c5d663a4c350c58ffef782506465e95
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-10 13:31:59 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (68.40%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,68 +1,35 @@
 class Solution {
 public:
     
-int fillMatrix(int i, int n, vector<vector<int>> &ans,int x,int y, char ch)
-    {
-        if(x<0 || y<0 || x>=n || y>=n || ans[x][y]!=-1)
-            return -1;
-        
-        ans[x][y]=i;
-    //cout<<x<<y<<i<<endl;
-    
-    if(ch=='R')
-        y=y+1;
-    else if(ch=='L')
-        y=y-1;
-    else if(ch =='U')
-        x=x-1;
-    else
-        x=x+1;
-        
-        int temp= fillMatrix(i+1,n,ans,x,y,ch);
-        
-        if(temp==-1)
-        {
-            if(ch=='R')
-            {
-                ch='D';
-                y=y-1;
-                x=x+1;
-            }
-            else if(ch=='L')
-            {
-                ch='U';
-                y=y+1;
-                x=x-1;
-            }
-            else if(ch =='U')
-            {
-               ch='R'; 
-                x=x+1;
-                y=y+1;
-            }
-            else
-            {
-                ch='L';
-                x=x-1;
-                y=y-1;
-            }
-            
-            fillMatrix(i+1,n,ans,x,y,ch);
-        }
-    
-      return 0;
-    }
-    
-    
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> ans(n, vector<int> (n,-1));
-        
-//         int ans[n][n];
-//         memset(ans,-1, sizeof(ans));
-        
-        fillMatrix(1,n,ans,0,0,'R');
-        
-        return ans;
-        
+       int r1 = 0, r2 = n-1;
+        int c1 = 0, c2 = n-1;
+        int val = 0;
+		
+		// result matrix
+        vector<vector<int>> v(n, vector<int> (n));
+        while(r1 <= r2 && c1 <= c2)
+        {
+            // left to right (row will be fixed)
+            for(int i = c1; i <= c2; ++i)
+                v[r1][i] = ++val;
+				
+				// move down(col will be fixed)
+            for(int i = r1+1; i <= r2; ++i)
+                v[i][c2] = ++val;
+				
+            
+                for(int i = c2-1; i>c1; --i)
+                    v[r2][i] = ++val;
+					
+					for(int i = r2; i>r1; --i) 
+                    v[i][c1] = ++val;
+    
+            ++r1;
+            --r2;
+            ++c1;
+            --c2;
+        }
+        return v;
     }
 };

ecd9c142f3ff14f931a31525b8d314edeff28390
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-10 11:16:25 -0700

    Time: 0 ms (100.00%), Space: 6.4 MB (58.80%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+     
+public:
+     int sum(vector<int> &v)
+    {
+        int s=0;
+        for(auto x:v)
+            s+=x;
+            
+        return s;
+    }
+    vector<vector<int>> ans;
+    void findCombination(int prev, int n,int k, vector<int> &temp)
+    {
+        if(k==0)
+        {
+            int t= sum(temp);
+            if(t==n)
+            {
+                cout<<"hb";
+                ans.push_back(temp);
+            }
+                
+            return;
+        }
+        
+        for(int i=prev;i<=9;i++)
+        {
+            temp.push_back(i);
+            findCombination(i+1,n,k-1,temp);
+            
+            temp.pop_back();
+        }
+        
+        
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> temp;
+       findCombination(1,n,k,temp);
+        
+        return ans;
+        
+    }
+};

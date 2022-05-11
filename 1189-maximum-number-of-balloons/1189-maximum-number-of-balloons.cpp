b27ceebb24802127f09d7acc0bd1ace31d9c3301
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-11 13:10:21 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (45.45%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        
+        vector<int> v(26,0);
+        string s="balon";
+        
+        for(int i=0;i<text.length();i++)
+        {
+            v[ text[i] - 'a' ]+=1;
+        }
+        
+        int ans=INT_MAX;
+        v['l'-'a']=v['l'-'a']/2;
+        v['o'-'a']=v['o'-'a']/2;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            ans=min(ans,v[s[i]-'a']);
+        }
+        
+        // if(v['l'-'a']<2 || v['o'-'a']<2) ans=0;
+        
+        if(ans==INT_MAX)
+            return 0;
+        else
+            return ans;
+        
+    }
+};

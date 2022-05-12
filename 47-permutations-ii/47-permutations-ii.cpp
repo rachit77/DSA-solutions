4ef75af7fddcbb8b60eb9ca76f99a9b18bea3361
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-12 07:18:08 -0700

    Time: 9 ms (67.44%), Space: 9.7 MB (40.51%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    
+    // APPROACH 1 : C ORRECT BUT MEMORY LIMIT EXCEEDED
+//     vector<vector<int>> ans;
+    
+//     void find(map<int,int> m, int count, vector<int> v, int n)
+//     {
+//         if(count == n) {
+//             ans.push_back(v);
+//             return;
+//         }
+        
+//         for(auto i:m) {
+//             if(i.second !=0)
+//             {
+//                 v.push_back(i.first);
+//                 i.second--;
+                
+//                 find(m, count++,v,n);
+//             }
+//         }
+//         return;
+//     }
+    
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+//         //make a hash map
+//         map<int,int> m;
+        
+//         for(auto x:nums) 
+//             m[x]++;
+        
+//         vector<int> v;
+//         int n=nums.size();
+        
+//         find(m,0,v,n);
+        
+//         return ans;
+//     }
+
+vector<vector<int>> ans;
+    
+    void permutate(vector<int> nums, int idx)
+    { 
+      if(idx==nums.size())
+      {
+          ans.push_back(nums);
+          return;
+      }
+        
+        for(int i=idx;i<nums.size();i++) {
+            
+            if(i!=idx && nums[i] == nums[idx]) 
+                continue;
+                
+            swap(nums[i],nums[idx]);
+            permutate(nums, idx+1);
+        }
+        return;
+    }
+
+vector<vector<int>> permuteUnique(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    permutate(nums,0);
+    
+    return ans;
+   }
+};

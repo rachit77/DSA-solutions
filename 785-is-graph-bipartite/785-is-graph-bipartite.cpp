a7690a93d0d40319d2b937532c1954a92a3c8864
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-16 10:07:57 -0700

    Time: 35 ms (41.30%), Space: 14.7 MB (11.73%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -20,19 +20,68 @@ public:
     
     bool isBipartite(vector<vector<int>>& graph) {
         
-        // using dfs
-        int n=graph.size();
-        vector<int> col(n,-1);
+//         // using dfs
+         int n=graph.size();
+//         vector<int> col(n,-1);
         
-        for(int i=0;i<n;i++) {
-            if(col[i]==-1)
+//         for(int i=0;i<n;i++) {
+//             if(col[i]==-1)
+//             {
+//                 col[i]=1;
+//                 if(!dfs(i,col,graph))
+//                     return false;
+//             }
+//         }
+//         return true;
+        
+        
+        
+//           vector<vector<int>> adj(n+1);
+        
+//         for(int i=0;i<dislikes.size();i++)
+//         {
+//             adj[dislikes[i][0]].push_back(dislikes[i][1]);
+//         }
+        
+        // bipartite graph using bfs
+        vector<int> vis(n+1,-1);
+        
+        int cnt=0;
+        
+        // graph can be disconnected
+        for(int i=0;i< n;i++)
+        {
+            queue<int> q;
+            if(vis[i]==-1) // node not visited
             {
-                col[i]=1;
-                if(!dfs(i,col,graph))
-                    return false;
+                vis[i]=1;
+                q.push(i);
+                while(!q.empty())
+                {
+                  int node=q.front();
+                    q.pop();
+                    
+                    for(auto x:graph[node])
+                    {
+                        if(vis[x]==-1)
+                        {
+                            vis[x]=1-vis[node];
+                            q.push(x);
+                        }
+                        else if(vis[node] == vis[x])
+                        {
+                            cout<<vis[node]<<" "<<i<<endl;
+                           return false; 
+                        }
+                            
+                    }
+                }
             }
         }
+        
         return true;
+        
+        
         
     }
 };

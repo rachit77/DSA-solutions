83237bb22f95d41af597f53da2152dd8263d1dc3
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-19 00:19:20 -0700

    Time: 54 ms (71.93%), Space: 15.9 MB (81.29%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
+    
+    int dx[4] = {0, -1, 1, 0};
+    int dy[4] = {1, 0, 0, -1};
+    
 int find(int i,int j, vector<vector<int>> &rec,vector<vector<int>>& matrix, int par)
 {
     int r=matrix.size();
     int c= matrix[0].size();
     
     //if out of bounds return 0
-    if(i<0 || j<0 || i>=r || j>=c) return 0;
-    
-    if(par>= matrix[i][j]) return 0;
+    if(i<0 || j<0 || i>=r || j>=c || par>= matrix[i][j]) return 0;
     
     //if already visited
     if( rec[i][j] !=-1 )
         return rec[i][j];
     
-    //left
-    int a=1+find(i,j-1,rec,matrix,matrix[i][j]);
-    
-    //right
-    a=max(a,1+find(i,j+1,rec,matrix,matrix[i][j]));
-    
-    //up
-    a=max(a,1+find(i-1,j,rec,matrix,matrix[i][j]));
-    
-    //down
-    a= max(a,1+find(i+1,j,rec,matrix,matrix[i][j]));
-    
+    int a=INT_MIN;
+    for (int k=0; k<4; k++)
+    {
+        int I = i + dx[k];
+        int J = j + dy[k];
+        
+        a=max(a,1+find(I,J,rec,matrix,matrix[i][j]));
+    }
     return rec[i][j]=a;
 }
     

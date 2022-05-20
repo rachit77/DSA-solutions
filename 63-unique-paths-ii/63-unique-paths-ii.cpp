9f534ecd7afa1220cf7c8d842d1bda74c243ad22
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-20 01:31:25 -0700

    Time: 6 ms (36.45%), Space: 7.9 MB (13.17%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -4,21 +4,16 @@ public:
     {
         int r= grid.size();
         int c= grid[0].size();
-        //cout<<r<<c<<endl;
     
         if(i<0 || j<0 || i>=r || j>=c || grid[i][j]==1) return 0;
         
         if(vis[i][j]!=-1) return vis[i][j];
         
         if(i== grid.size()-1 && j==grid[0].size()-1)
-        {
             return 1;
-        }
         
         int a= find(i,j+1,vis,grid); //right
         int b= find(i+1,j,vis,grid);  //down
-        
-        //cout<<i<<" "<<j<<" "<<a+b<<endl;
             
         return vis[i][j]=a+b;    
     }

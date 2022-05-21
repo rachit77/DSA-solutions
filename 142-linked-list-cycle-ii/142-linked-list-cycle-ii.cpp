a5dea824cc951d57003d2b6ec81fa0ac9d60536a
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-21 14:37:00 -0700

    Time: 16 ms (26.26%), Space: 9.6 MB (11.56%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        //method-1 can use a hash map
+        //method-2 slow and fast pointer
+        
+        unordered_map<ListNode*,int> m;
+        
+        ListNode *s=head,*f=head;
+        while(f!=NULL && f->next!=NULL)
+        {
+            if(m[s]==1)
+            {
+                return s;
+            }
+            m[s]=1;
+            
+            s=s->next;
+            f=f->next->next;
+        }
+        
+        return NULL;
+    }
+};

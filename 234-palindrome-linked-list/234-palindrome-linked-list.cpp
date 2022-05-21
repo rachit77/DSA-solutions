82a4f0dd51834917ff74bf2fcfff3bd6b3232dea
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-21 14:18:49 -0700

    Time: 205 ms (94.94%), Space: 110.4 MB (94.82%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -15,22 +15,49 @@ public:
         // method-1 take last half ans reverse it to check with first half
         // method-2 use a vector to push ele of linked list and check
         
-        vector<int> v;
-        ListNode* temp=head;
-        int cnt=0;
-        while(temp)
+        // method-2. sc= o(n)
+//         vector<int> v;
+//         ListNode* temp=head;
+//         int cnt=0;
+//         while(temp)
+//         {
+//             v.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         int n=v.size();
+//         for(int i=0;i<n/2;i++)
+//         {
+//             if(v[i]!=v[n-i-1])
+//                 return false;
+//         }
+        
+//         return true;
+        
+        //method-1 reverse the first half
+        ListNode *s=head,*f=head, *p=NULL;
+        
+        while(f!=NULL && f->next!=NULL)
         {
-            v.push_back(temp->val);
-            temp=temp->next;
+            f=f->next->next;
+            ListNode* temp=s;
+            s=s->next;
+            temp->next=p;
+            p=temp;
+            
         }
-        int n=v.size();
-        for(int i=0;i<n/2;i++)
+        
+        if(f) s=s->next; //if odd length
+        
+        while(s)
         {
-            if(v[i]!=v[n-i-1])
-                return false;
+            if(p->val != s-> val) return false;
+            
+            p=p->next;
+            s=s->next;
         }
         
         return true;
+        
         
     }
 };

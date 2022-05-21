1a013b40f7521961fc8bf80105de65f41bd53677
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-21 14:37:31 -0700

    Time: 23 ms (9.19%), Space: 9.7 MB (8.95%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -13,21 +13,18 @@ public:
         //method-1 can use a hash map
         //method-2 slow and fast pointer
         
+        //method-1
         unordered_map<ListNode*,int> m;
-        
         ListNode *s=head,*f=head;
         while(f!=NULL && f->next!=NULL)
         {
             if(m[s]==1)
-            {
                 return s;
-            }
             m[s]=1;
             
             s=s->next;
             f=f->next->next;
-        }
-        
+        } 
         return NULL;
     }
 };

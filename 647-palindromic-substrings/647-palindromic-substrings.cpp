88bdc98db6ddd41ceb29db457d6dd45e8a135286
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-22 02:14:39 -0700

    Time: 0 ms (100.00%), Space: 6.1 MB (99.80%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //method-1 naive approach brute force
+    
 //     bool checkPalindrome(int i,int j, string s)
 //     {
 //         for(int t=0;t<=(j-i)/2;t++)
@@ -10,7 +12,7 @@ public:
 //     }
     // int countSubstrings(string s) {
         
-//         //method-1 naive approach brute force
+//         
 //         int n=s.length(),ans=0;
         
 //         for(int i=0;i<n;i++)

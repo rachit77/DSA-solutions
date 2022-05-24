85dbaf30830bac6f9117c57a2a0404f8a3fdc94b
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-24 02:35:06 -0700

    Time: 186 ms (8.94%), Space: 58.9 MB (98.35%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       int i,j,n=height.size();
+        int ans=0;
+
+// method-1 brute force (tle)     
+//         for(i=0;i<n;i++)
+//         {
+//             for(j=i+1;j<n;j++)
+//             {
+//                 int h=min(height[i],height[j]);
+//                 int temp=h*(j-i);
+                
+//                 ans=max(ans,temp);
+//             }
+//         }       
+//         return ans;
+        
+ // method-2 gredy approach
+        int l=0,r=n-1,h=0;
+        
+        while(l<r)
+        {
+            h=min(height[l],height[r]);
+            int area=h*(r-l);
+            
+            ans=max(ans,area);
+            if(height[l]<=height[r])
+                l++;
+            else
+                r--;
+        }
+        return ans;
+        
+    }
+};

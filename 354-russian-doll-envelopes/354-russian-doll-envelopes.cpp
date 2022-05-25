55558ffb3702fb050e59f56c6dcc4e0d04e007c3
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-25 08:43:35 -0700

    Time: 443 ms (65.79%), Space: 77.7 MB (44.39%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int binary_search(vector<int> &lis, int ele,int l, int h)
+    {
+        while(l<h)
+        {
+            int mid=l+(h-l)/2;
+            
+            if(lis[mid]==ele) return mid;
+            
+            else if(ele> lis[mid])
+            {
+                l=mid+1;
+            }
+            else
+                h=mid-1;
+        }
+        return l;
+    }
+    
+    
+    
+    
     static bool comp(vector<int> &a, vector<int> &b)
     {
         if(a[0]==b[0]) return a[1]>b[1];
@@ -30,14 +51,28 @@ public:
  
         //2nd method
         vector<int> lis;
-        for(int i=0;i<n;i++)
+        lis.push_back(envelopes[0][1]);
+        for(int i=1;i<n;i++)
         {
+            
             int ele= envelopes[i][1];
             int idx= lower_bound(lis.begin(), lis.end(), ele) - lis.begin();
             
-            if(idx==lis.size()) lis.push_back(ele);
+           // if(idx==lis.size())
+            
+            if(ele > lis[lis.size()-1]) //not present
+            lis.push_back(ele);
+            
             else
+            {
+                //int h=lis.size()-1;
+                //int idx= binary_search(lis,ele,0,h);
                 lis[idx]=ele;
+            }
+            
+            // if(idx==lis.size()) lis.push_back(ele);
+            // else
+            //     lis[idx]=ele;
         }
         return lis.size();
         

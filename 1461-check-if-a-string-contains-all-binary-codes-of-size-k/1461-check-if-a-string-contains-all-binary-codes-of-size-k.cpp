fbdb4e8851c21536a05d907a0d0942c6446553e4
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-30 20:51:57 -0700

    Time: 991 ms (21.69%), Space: 91.5 MB (15.72%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        
+        // can use set or map
+        
+        int n=s.length();
+    
+        // 2^k is the total num 
+        int num=1;
+        for(int i=1;i<=k;i++)
+            num=2*num;
+        
+        
+        if(n<=k) return false;
+        
+        set<string> st;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i+k<=n)
+            {
+               string temp= s.substr(i,k);
+                st.insert(temp);
+            }
+            else break;
+            
+        }
+        cout<<st.size()<<endl;
+        cout<<num;
+            
+        if(num== st.size())
+            return true;
+        
+        return false;
+        
+    }
+};

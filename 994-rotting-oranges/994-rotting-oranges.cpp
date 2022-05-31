a94c19c5855fcc5062c16798aab10a93831c6d4c
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-31 07:34:47 -0700

    Time: 11 ms (53.15%), Space: 13.1 MB (79.86%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+  struct Node{
+        int time;
+        int x;
+        int y;
+    };
+    int orangesRotting(vector<vector<int>>& grid) {
+       
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<Node> q;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    struct Node x = {0, i, j};
+                    q.push(x);
+                }
+            }
+        }
+        
+        
+        struct Node x ={0,0,0};
+        
+        while(!q.empty())
+        {
+            x = q.front(); 
+            //rot all the adjacent nodes and push it in the queue 
+            q.pop();
+            int time = x.time;
+            int curi = x.x;
+            int curj = x.y;
+            
+            // cout << time << " "<< curi <<" "<< curj << endl;
+            if(curj > 0)
+            {
+                if(grid[curi][curj-1] == 1)
+                {
+                    grid[curi][curj-1] = 2;
+                    struct Node y = {time+1, curi, curj-1};
+                    q.push(y);
+                }
+            }
+            if(curi > 0)
+            {
+                if(grid[curi-1][curj] == 1)
+                {
+                    grid[curi-1][curj] = 2;
+                    struct Node y = {time+1, curi-1, curj};
+                    q.push(y);
+                }
+            }
+            if(curi < n-1)
+            {
+                if(grid[curi+1][curj] == 1)
+                {
+                    grid[curi+1][curj] = 2;
+                    struct Node y = {time+1, curi+1, curj};
+                    q.push(y);
+                }
+            }
+            if(curj < m-1)
+            {
+                if(grid[curi][curj+1] == 1)
+                {
+                    grid[curi][curj+1] = 2;
+                    struct Node y = {time+1, curi, curj+1};
+                    q.push(y);
+                }
+            }
+        }
+        
+        //check the grid
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == 1)
+                    return -1;
+            }
+        }
+        return x.time;
+    
+    }
+};

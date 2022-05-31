fef1cabbde9914da6a098ae540c9de05698ccb28
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-05-30 21:17:07 -0700

    Time: 699 ms (29.24%), Space: 86.8 MB (24.21%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        
-        // can use set or map
-        
+
         int n=s.length();
-    
-        // 2^k is the total num 
-        int num=1;
-        for(int i=1;i<=k;i++)
-            num=2*num;
+
+        unordered_set<string> st;
         
-        
-        if(n<=k) return false;
-        
-        //unordered_set<string> st;
-        unordered_map<string,int> st;
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n-k;i++)
         {
-            if(i+k<=n)
-            {
                string temp= s.substr(i,k);
-               // st.insert(temp);
-                st[temp]=1;
-            }
-            else break;
-            
+               st.insert(temp);
+                
         }
         
-        // int count=0;
-        // for(auto i:st)
-        //     count++;
-            
-        if(num== st.size())
+        if((1<<k) == st.size())
             return true;
        
         return false;

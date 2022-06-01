ce4d6a7488d8ff9f305defb79676a10449cdbefe
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-01 06:34:40 -0700

    Time: 0 ms (100.00%), Space: 9.5 MB (20.12%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -2,25 +2,42 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
-        int n=strs.size();
-        int cnt=0,flag=1;
-        for(int i=0;i<strs[0].length();i++)
-        {
-            char ch=strs[0][i];
-            if(flag){
-            for(int j=1;j<n;j++)
-            {
-                if(strs[j].length() <= i ||  ch!= strs[j][i])
-                {
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag)
-            cnt++;
-            }
-        }
+//         int n=strs.size();
+//         int cnt=0,flag=1;
+//         for(int i=0;i<strs[0].length();i++)
+//         {
+//             char ch=strs[0][i];
+//             if(flag){
+//             for(int j=1;j<n;j++)
+//             {
+//                 if(strs[j].length() <= i ||  ch!= strs[j][i])
+//                 {
+//                     flag=0;
+//                     break;
+//                 }
+//             }
+//             if(flag)
+//             cnt++;
+//             }
+//         }
         
-        return strs[0].substr(0,cnt);
+//         return strs[0].substr(0,cnt);
+        
+        
+        //method-2 tc: nlog(n)
+        sort(strs.begin(),strs.end());
+        string ans="";
+        string a=strs[0];
+        string b=strs[strs.size()-1];
+        int cnt=0;
+        for(int i=0;i<a.length();i++)
+        {
+            if(a[i] == b[i])
+            {
+                ans=ans+ a[i];
+            }
+            else break;
+        }
+        return ans;
     }
 };

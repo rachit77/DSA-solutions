d92a81a60c581551de2f42a51b13861f2d1091b8
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-07 09:09:31 -0700

    Time: 7 ms (50.39%), Space: 15 MB (29.30%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void find(int i, int o, int c,int n,string s, vector<string> &ans)
+    {
+        if(c>o || o>n || c>n) return;
+        
+        if(o==c && o==n) 
+        {
+            ans.push_back(s);
+            return;
+        }
+        
+        find(i+1,o+1,c,n,s+"(",ans);
+        find(i+1,o,c+1,n,s+")",ans);
+            
+        return;    
+    }
+    vector<string> generateParenthesis(int n) {
+        
+        vector<string> ans;
+        int o=0,c=0;
+        string s="";
+        find(0,o,c,n,s,ans);
+        return ans;
+        
+    }
+};

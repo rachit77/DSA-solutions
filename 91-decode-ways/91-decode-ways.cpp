6535f4f8cb30fea8e5865c4b526011ed27e6fcf7
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-07 05:28:24 -0700

    Time: 3 ms (56.60%), Space: 6.2 MB (72.17%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,32 +1,76 @@
 class Solution {
 public:
-    int find(string s, int i, int dp[])
-    {
-        if(dp[i]!=-1) return dp[i];
+    //method-1 memmoization
+//     int find(string s, int i, int dp[])
+//     {
+//         if(dp[i]!=-1) return dp[i];
         
-        if(i==s.length()) return 1;
+//         if(i==s.length()) return 1;
+        
+//         int s1,s2;
+//         int a=0,b=0;
+        
+//         s1=s[i]-'0';
+//         if(s1!=0)
+//         a=find(s,i+1,dp);      
+        
+//         if(i<s.length()-1 )
+//            {
+//                s2=s[i+1]-'0';
+//             if(s1==1 || (s1==2 && s2<=6))
+//                b=find(s,i+2,dp);
+//            }
+        
+        
+//         return dp[i]=a+b;
+//     }
+//     int numDecodings(string s) {
+//         int dp[s.length()+1];
+//         memset(dp,-1,sizeof(dp));
+//         return find(s,0,dp);
+        
+//     }
+    
+    //method-2 tabulation
+    int numDecodings(string s) {
+        int n=s.length();
+        vector<int> dp(n+1,0);
+        
+       // dp[0]=1;
         
         int s1,s2;
-        int a=0,b=0;
+        s1=s[0]-'0';
+        if(s1!=0) dp[0]=1;
+        //cout<<dp[1]<<endl;
         
-        s1=s[i]-'0';
-        if(s1!=0)
-        a=find(s,i+1,dp);      
+        if(n>=1)
+        {
+            s2=s[1]-'0';
+            if(s2!=0) dp[1]=dp[0];
+            
+            if(s1==1 || (s1==2 &&  s2<=6))
+            {
+              dp[1]+=1;
+            }
+                
+        }
         
-        if(i<s.length()-1 )
-           {
-               s2=s[i+1]-'0';
-            if(s1==1 || (s1==2 && s2<=6))
-               b=find(s,i+2,dp);
-           }
-        
-        
-        return dp[i]=a+b;
-    }
-    int numDecodings(string s) {
-        int dp[s.length()+1];
-        memset(dp,-1,sizeof(dp));
-        return find(s,0,dp);
-        
+        for(int i=2;i<n;i++)
+        {
+            int a=0,b=0;
+            s1=s[i]-'0';
+            if(s1!=0) 
+                a=dp[i-1];
+            
+            s2=s[i-1]-'0';
+            if(s2==1 || (s2==2 && s1<=6))
+            {
+                b=dp[i-2];
+            }
+            dp[i]=a+b;
+            
+            // cout<<a<<" "<<b<<endl;
+        }
+        return dp[n-1];
     }
 };

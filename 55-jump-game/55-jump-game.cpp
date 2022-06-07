482e1e220e2a9015a4ec170e874101976b6cbea8
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-07 06:37:17 -0700

    Time: 119 ms (32.74%), Space: 48.4 MB (44.17%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    bool find(int i,vector<int>& nums, int dp[])
-    {
-        if(dp[i]!=-1) return dp[i];
-        if(i>=nums.size()-1) return true;
+    // method-1 memoization
+//     bool find(int i,vector<int>& nums, int dp[])
+//     {
+//         if(dp[i]!=-1) return dp[i];
+//         if(i>=nums.size()-1) return true;
         
-        for(int t=1;t<=nums[i];t++)
-        {
-            if( find(i+t, nums,dp) ) return dp[i]=true;
-        }
-        return dp[i]=false;
-    }
+//         for(int t=1;t<=nums[i];t++)
+//         {
+//             if( find(i+t, nums,dp) ) return dp[i]=true;
+//         }
+//         return dp[i]=false;
+//     }
+//     bool canJump(vector<int>& nums) {
+//         int dp[nums.size()];
+//         memset(dp,-1,sizeof(dp));
+//         return find(0,nums,dp);
+//     }
+    
+    // method-2 O(n) time 
     bool canJump(vector<int>& nums) {
-        int dp[nums.size()];
-        memset(dp,-1,sizeof(dp));
-        return find(0,nums,dp);
+        int idx=0,i;
+        
+        for(i=0;i<nums.size();i++)
+        {
+            if(i>idx) return false;
+                
+                idx=max(idx,i+nums[i]);
+            
+            if(i>=nums.size()-1) return true;
+        }
+        
+     return false;   
+        
     }
 };

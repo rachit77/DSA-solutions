a701d02501024be271eb3095cbab5fec8eebed8f
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-07 02:45:52 -0700

    Time: 189 ms (67.83%), Space: 9 MB (61.95%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
+    //method-1 memoization
+//     int find(int n, int dp[])
+//     {
+//         if(dp[n]!=-1) return dp[n];
+        
+//         if(n==0) return 0;
+        
+//         int i=1,ans=INT_MAX;
+        
+//         while(i*i <=n)
+//         {
+//            int k= find(n- i*i,dp);
+//             ans=min(ans,k+1);
+//             i++;
+//         }
+//         return dp[n]=ans;
+//     }
+//     int numSquares(int n) {
+        
+//         int dp[n+1];
+//         memset(dp,-1,sizeof(dp));
+//         return find(n,dp);
+        
+//     }
     
-    int find(int n, int dp[])
-    {
-        if(dp[n]!=-1) return dp[n];
+    
+    int numSquares(int n){
+        //method-2 dp
+        vector<int> dp(n+1,INT_MAX);
+        dp[0]=0;
         
-        if(n==0) return 0;
-        
-        int i=1,ans=INT_MAX;
-        
-        while(i*i <=n)
+        int cnt=1;
+        while(cnt*cnt <=n)
         {
-           int k= find(n- i*i,dp);
-            ans=min(ans,k+1);
-            i++;
+            int temp=cnt*cnt;
+            for(int i=temp;i<=n;i++)
+            {
+                dp[i]=min(dp[i], 1+dp[i-temp]);
+            }
+            cnt++;
         }
-        return dp[n]=ans;
-    }
-    int numSquares(int n) {
         
-        int dp[n+1];
-        memset(dp,-1,sizeof(dp));
-        return find(n,dp);
-        
+        return dp[n];
     }
 };

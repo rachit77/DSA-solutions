0742d4fd0bbc26df5299dfe4fa2f3133f05079b4
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-08 04:47:56 -0700

    Time: 18 ms (14.28%), Space: 17.5 MB (5.66%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    //method-3
+   int find(int i,int hold,vector<int> &prices, vector<vector<int>> &dp)
+   {
+       if(i==prices.size()) return 0;
+       
+       if(dp[i][hold]!=-1) return dp[i][hold];
+       
+       int profit;
+       
+       if(hold==0) //not holding any stock
+       {
+           //case 1 : buy the current stock
+           int a=find(i+1,1,prices,dp) -prices[i];
+           
+           //case 2: skip the stock
+           int b=find(i+1,0,prices,dp);
+           
+           profit=max(a,b);
+           
+       }
+       else  //holding a stock
+       {
+           //case 1: sell the stock
+           int a=find(i+1,0,prices,dp)+prices[i];
+           
+           //case 2: skip the stock
+           int b=find(i+1,1,prices,dp);
+           
+           profit=max(a,b);
+       }
+       
+       return dp[i][hold]=profit;
+   }
+    
     int maxProfit(vector<int>& prices) {
         //method-1 GREEDY APPROACH
         // stack<int> st;
@@ -17,11 +51,16 @@ public:
         // return profit;
         
         //method-2 valley peak approach
-        int ans=0,i;
-        for(i=1;i<prices.size();i++)
-        {
-            ans+=max(prices[i]-prices[i-1],0);
-        }
-        return ans;
+        // int ans=0,i;
+        // for(i=1;i<prices.size();i++)
+        // {
+        //     ans+=max(prices[i]-prices[i-1],0);
+        // }
+        // return ans;
+        
+        //method-3 recursion and memoization
+        
+        vector<vector<int>> dp(prices.size(), vector<int>(2,-1));
+        return find(0,0,prices,dp);
     }
 };

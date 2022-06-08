a272b22e3565f1d314ba9ae577fbf8878385c2ed
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-08 03:54:54 -0700

    Time: 9 ms (58.41%), Space: 13 MB (53.88%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        stack<int> st;
+        //method-1 GREEDY APPROACH
+        // stack<int> st;
+        // int profit=0;
+        // for(int i=0;i<prices.size();i++)
+        // {
+        //     if(!st.empty() && prices[i]>st.top())
+        //     {
+        //         profit+= prices[i]-st.top();
+        //         while(!st.empty() && prices[i]>=st.top())
+        //             st.pop();
+        //     }
+        //      st.push(prices[i]);    
+        // }
+        // return profit;
         
-        int profit=0;
-        for(int i=0;i<prices.size();i++)
+        //method-2 valley peak approach
+        int ans=0,i;
+        for(i=1;i<prices.size();i++)
         {
-            if(!st.empty() && prices[i]>st.top())
-            {
-                profit+= prices[i]-st.top();
-                
-                while(!st.empty() && prices[i]>=st.top())
-                    st.pop();
-            }
-             st.push(prices[i]);
-            
+            ans+=max(prices[i]-prices[i-1],0);
         }
-        
-        return profit;
+        return ans;
     }
 };

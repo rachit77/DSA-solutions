e3b733ccebd866b176d5b2ce9a759dd5d71825ae
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-09 03:54:46 -0700

    Time: 0 ms (100.00%), Space: 7.6 MB (6.87%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -43,6 +43,13 @@ public:
 //         return dummy->next;
         
         //method-2 recursive
-        return arrange(head);
+        if(!head || !head->next) return head;
+        
+        ListNode *temp=head->next;
+        head->next=swapPairs(head->next->next);
+        temp->next=head;
+        
+        return temp;
+       // return arrange(head);
     }
 };

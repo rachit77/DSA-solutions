696bac697409e1e923f4ae2f8ec0165e3c8b5e54
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-12 16:21:41 +0530

    Time: 322 ms (79.71%), Space: 103.7 MB (59.54%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int> pre(n+1,0);
+        
+        for(int i=1;i<=n;i++)
+            pre[i]=pre[i-1]+ nums[i-1];
+        
+        unordered_map<int,int> mp;  //num, index
+        
+        int sum_sub=0, cur_sum=0, st=0;
+        for(int i=0;i<n;i++)
+        {
+           if(mp.count(nums[i])>0 ) // number already exist
+           {
+               if(mp[nums[i]]>=st)
+               {
+                   sum_sub=max(sum_sub, cur_sum);
+                   cur_sum=pre[i+1]-pre[ mp[nums[i]] +1];
+                    st=mp[nums[i]]+1;
+                   
+                   
+                    mp[nums[i]]=i;
+               }
+               else
+               {
+                   cur_sum+=nums[i];
+                   mp[nums[i]]=i;
+               }
+               
+           }
+            else
+            {
+                cur_sum+=nums[i];
+                mp[nums[i]]=i;
+            }
+        }
+        
+         cout<<st;
+         sum_sub=max(sum_sub, cur_sum);
+        
+        return sum_sub;
+        
+    }
+};

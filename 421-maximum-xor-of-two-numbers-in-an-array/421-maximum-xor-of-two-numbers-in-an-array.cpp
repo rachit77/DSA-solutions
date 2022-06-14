4f25bb73a0e219c4b39ead4b05c3fdd6fc62dfe4
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-15 01:11:39 +0530

    Time: 265 ms (89.99%), Space: 79.3 MB (34.45%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    struct TrieNode {
+        struct TrieNode* child[2];
+        int val;
+        
+        TrieNode(){
+            child[0]=NULL;
+            child[1]=NULL;
+        }
+    };
+    
+    TrieNode* node;
+    
+    void insert(int num)
+    {
+        TrieNode* temp=node;
+        for(int i=31;i>=0;i--)
+        {
+            bool set= num& (1<<i);
+            if(temp->child[set]==NULL)
+            {
+                temp->child[set]=new TrieNode();
+            }
+            temp=temp->child[set];
+        }
+        temp->val=num;
+    }
+    
+    int findMax(int num)
+    {
+        TrieNode* temp=node;
+        
+        int k=0;
+        for(int i=31;i>=0;i--)
+        {
+            bool set= num&(1<<i);
+            
+            if(temp->child[!set]!=NULL) //opposite bits
+            {
+                temp=temp->child[!set];
+                 k=k+ (1<<i);
+            }
+            else if(temp->child[set]!=NULL)
+            {
+                temp=temp->child[set];
+            }
+           // k=k+ (1-set)*(1<<i);
+        }
+        // return num ^temp->val;
+        return k;
+    }
+    int findMaximumXOR(vector<int>& nums) {
+        
+        //naive approach tc:O(n^2)
+       // int ans=0,i,j,n=nums.size();
+        
+        // for(i=0;i<n-1;i++)
+        // {
+        //     for(j=i+1;j<n;j++)
+        //     {
+        //        int temp=nums[i] ^ nums[j];
+        //         ans=max(ans,temp);
+        //     }
+        // }
+            
+        //return ans;
+       
+        
+        //method-2 trie
+        
+        node=new TrieNode();
+        int i,n=nums.size();
+        if(n<=1) return 0;
+        int ans=INT_MIN;
+        insert(nums[0]);
+        
+        for(int i=1;i<n;i++)
+        {
+            int temp=findMax(nums[i]);
+            ans=max(ans,temp);
+            insert(nums[i]);
+        }
+        
+        return ans;
+    }
+};

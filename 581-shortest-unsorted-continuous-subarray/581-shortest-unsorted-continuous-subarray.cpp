06c9c5f7e9afb4d06a25e74c9f06431a296f461d
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-14 23:13:33 +0530

    Time: 29 ms (94.56%), Space: 26.5 MB (88.49%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -2,60 +2,55 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         
-        //method-1
-        int n=nums.size();
-        vector<int> v(nums.begin(),nums.end());
+        //method-1 nlogn
+//         int n=nums.size();
+//         vector<int> v(nums.begin(),nums.end());
         
-        sort(v.begin(),v.end());
+//         sort(v.begin(),v.end());
         
-        int a=0,b=0;
+//         int a=0,b=0;
         
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=v[i])
-            {
-                a=i;
-                break;
-            }
-        }
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            if(nums[i]!=v[i])
-            {
-                b=i;
-                break;
-            }
-        }
-        
-        if(a==b) return 0;
-        return b-a+1;
-        
-        //approch-2
-        // find first and last point of diff
-        
-//         int i,n=nums.size();
-        
-//         //find left point
-//         int l_max=nums[0],st=0;
-//         for(i=0;i<n;i++)
+//         for(int i=0;i<n;i++)
 //         {
-//             if(nums[i]>l_max)
+//             if(nums[i]!=v[i])
 //             {
-//                 st=i;
-//                 l_max=nums[i];
-//             }
-//             else if(l_max==nums[i]) continue;
-//             else
-//             {
+//                 a=i;
 //                 break;
 //             }
 //         }
-//          cout<<st;
         
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             if(nums[i]!=v[i])
+//             {
+//                 b=i;
+//                 break;
+//             }
+//         }
         
+//         if(a==b) return 0;
+//         return b-a+1;
         
-//         //
-//         return 0;
+        //method-2 TC: O(N)
+        int n=nums.size(),i;
+        int l=0,h=n-1;
+        
+        while(l<n-1 && nums[l]<=nums[l+1]) l++;
+        while(h>0 && nums[h]>=nums[h-1]) h--;
+        
+        if(l==n-1) return 0;
+        
+        int arr_min=INT_MAX, arr_max=INT_MIN;
+        
+        for(int i=l;i<=h;i++)
+        {
+            arr_min=min(arr_min,nums[i]);
+            arr_max=max(arr_max,nums[i]);
+        }
+        
+        while(l-1>=0 && nums[l-1] > arr_min) l--;
+        while(h+1<n && nums[h+1]<arr_max) h++;
+        
+        return h-l+1;
     }
 };

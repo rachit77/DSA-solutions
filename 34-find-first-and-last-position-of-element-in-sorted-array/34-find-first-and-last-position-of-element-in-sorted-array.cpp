8dc43d68b2df373a21e9fcfcaa7803600618d2f6
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-16 23:25:17 +0530

    Time: 8 ms (81.50%), Space: 13.7 MB (63.38%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,81 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> searchRange(vector<int>& a, int target) {
         
-        //binary search
-        
-        int l=0,h=nums.size()-1,ans=-1;
-        
-        while(l<=h)
-        {
-            int mid =l+(h-l)/2;
+        //method-1 binary search
+//         int l=0,h=nums.size()-1,ans=-1;
+//         while(l<=h)
+//         {
+//             int mid =l+(h-l)/2;
             
-            if(nums[mid]==target)
-            {
-                ans=mid;
-                break;
-            }
+//             if(nums[mid]==target)
+//             {
+//                 ans=mid;
+//                 break;
+//             }
             
-            if(target<nums[mid])
-                h=mid-1;
-            else
-                l=mid+1;
-        }
+//             if(target<nums[mid])
+//                 h=mid-1;
+//             else
+//                 l=mid+1;
+//         }
         
-        vector<int> res(2,-1);
-        //cout<<ans;
-        if(ans!=-1)
-        {
-            int a=ans,b=ans;
+//         vector<int> res(2,-1);
+//         //cout<<ans;
+//         if(ans!=-1)
+//         {
+//             int a=ans,b=ans;
             
-            while(a>=1 && nums[a-1]==nums[ans]) a--;
-            while(b< nums.size()-1 && nums[b+1]==nums[ans]) b++;
+//             while(a>=1 && nums[a-1]==nums[ans]) a--;
+//             while(b< nums.size()-1 && nums[b+1]==nums[ans]) b++;
             
-            res[0]=a;
-            res[1]=b;
-        }
+//             res[0]=a;
+//             res[1]=b;
+//         }
         
-        return res;
+//         return res;
+        
+      int s = 0, e = a.size()-1;
+	vector<int> ans(2, -1);
+	//first occurrence
+	while(s <= e){
+		int m = s + (e-s)/2;
+		if(a[m] < target)
+			s = m+1;
+		else if(a[m] > target)
+			e = m-1;
+		else{
+			if(m == 0 || a[m] != a[m-1]){
+				ans[0] = m;
+				break;
+			}
+			else{
+				e = m-1;
+				ans[0] = m-1;
+			}
+		}
+	}
+
+	//last occurrence
+	s = 0, e = a.size()-1;
+	while(s <= e){
+		int m = s + (e-s)/2;
+		if(a[m] < target)
+			s = m+1;
+		else if(a[m] > target)
+			e = m-1;
+		else{
+			if(m == e || a[m] != a[m+1]){
+				ans[1] = m;
+				break;
+			}
+			else{
+				s = m+1;
+				ans[1] = m+1;
+			}
+		}
+	}
+	return ans;   
+        
     }
 };

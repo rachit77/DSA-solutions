2f61189444dc5ca30e03954f3418b1a9b5215474
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-16 11:52:42 +0530

    Time: 8 ms (10.56%), Space: 6 MB (10.54%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         
-        // uint32_t ans=0;
-        // for(int i=0;i<32;i++)
-        // {
-        //     if((n & (1<<i)))
-        //     {
-        //         ans=ans+ 1<<(31-i);
-        //     } 
-        // }
-        // return ans;
-        
-        
-        uint32_t res = 0;
-        for (int i = 0; i < 31; i++) {
-            res = (n % 2) + res << 1;
-            n >>= 1;
+        uint32_t ans=0;
+        for(int i=0;i<32;i++)
+        {
+            if((n & (1<<i)))
+            {
+                ans=ans+ (1<<(31-i));
+            } 
         }
-        return res + n % 2;
+        return ans;
+        
+        
+        // uint32_t res = 0;
+        // for (int i = 0; i < 31; i++) {
+        //     res = (n % 2) + res << 1;
+        //     n >>= 1;
+        // }
+        // return res + n % 2;
     }
 };

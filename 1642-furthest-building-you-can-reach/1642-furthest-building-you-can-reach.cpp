035864c3f15012d6275a1657d52ea68767b2e0dc
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-21 21:46:26 +0530

    Time: 553 ms (5.02%), Space: 93.8 MB (5.10%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -31,32 +31,72 @@ public:
 //         return ans-1;    
 //     }
     
-    //method-2 GREEDY with max heap
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+    //method-2  max heap
+//     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         
-        priority_queue<int> q;
-        int i;
-        for(i=0;i<heights.size()-1;i++)
+//         priority_queue<int> q;
+//         int i;
+//         for(i=0;i<heights.size()-1;i++)
+//         {
+//             int diff=heights[i+1]-heights[i];
+            
+//             if(diff<=0)
+//                 continue;
+            
+//             q.push(diff);
+//             bricks-=diff;
+            
+//             if(bricks<0)
+//             {
+//                 bricks+= q.top();
+//                 q.pop();
+//                 ladders--;
+//             }
+            
+//             if(ladders<0) break;
+//         }
+        
+//         return i;
+//     }
+    
+    
+    //method-3 Binary search
+    bool find(int mid,vector<int>& heights, int bricks, int ladders)
+    {
+        vector<int> v;
+        for(int i=0;i<mid;i++)
         {
-            int diff=heights[i+1]-heights[i];
-            
-            if(diff<=0)
-                continue;
-            
-            q.push(diff);
-            bricks-=diff;
-            
-            if(bricks<0)
-            {
-                bricks+= q.top();
-                q.pop();
-                ladders--;
-            }
-            
-            if(ladders<0) break;
+            int dif= heights[i+1] - heights[i];
+            if(dif>0)
+                v.push_back(dif);
         }
         
-        return i;
+        sort(v.begin(),v.end(), greater<int>());
+        
+        for(int i=ladders;i<v.size();i++)
+        {
+          bricks-=v[i];
+          if(bricks<0) return false;
+        }
+        return true;
     }
+     int furthestBuilding(vector<int>& heights, int bricks, int ladders)      {
+         int lo=0,h=heights.size()-1;
+         
+         while(lo<h)
+         {
+            // int mid= lo+(h-lo)/2;
+              int mid = (lo+h+1)/2;
+             bool isReachable= find(mid,heights,bricks,ladders);
+             
+             if(isReachable==true)
+                 lo=mid;
+             else
+                 h=mid-1;
+         }
+         
+         return lo;
+   }
+    
     
 };

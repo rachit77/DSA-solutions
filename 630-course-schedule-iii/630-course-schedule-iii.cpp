e94c8fd42b2400febec14778bab49579514e186a
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-23 14:23:20 +0530

    Time: 1384 ms (10.00%), Space: 56.2 MB (85.57%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -8,38 +8,78 @@ public:
     }
     int scheduleCourse(vector<vector<int>>& courses) {
         
-        //sort them acc to deadline
+        //sort them acc to ascending deadline
         sort(courses.begin(), courses.end(), comp);
+       
         
-        //make a max heap to push courses acc to duration
-        priority_queue<pair<int,int>> q;  // duration, deadline
+        //method-1
         
-        int duration=0,ans=0;
+//         //make a max heap to push courses acc to duration
+//         priority_queue<pair<int,int>> q;  // duration, deadline
+        
+//         int duration=0,ans=0;
+        
+//         for(int i=0;i<courses.size();i++)
+//         {
+//             duration+= courses[i][0];
+            
+//             if(duration>courses[i][1]) //pop out frm heap
+//             {
+//                 if(!q.empty() && courses[i][0]< q.top().first)
+//                 {
+//                     duration= duration-q.top().first;
+//                     q.pop();
+//                     q.push({courses[i][0],courses[i][1]});
+//                 }
+//                 else
+//                 {
+//                     duration= duration-courses[i][0];
+//                 }
+//             }
+//             else
+//             {
+//                 q.push({courses[i][0],courses[i][1]});
+//                 ans++;    
+//             }
+//         }
+        
+//         return q.size();
+    
+    //method-2 using an extra list
+        vector<int> valid;
+        int time=0;
         
         for(int i=0;i<courses.size();i++)
         {
-            duration+= courses[i][0];
-            
-            if(duration>courses[i][1]) //pop out frm heap
+            //cout<<i<<" "<<time<<endl;
+            if(time+courses[i][0]<=courses[i][1]) //valid entr
             {
-                if(!q.empty() && courses[i][0]< q.top().first)
-                {
-                    duration= duration-q.top().first;
-                    q.pop();
-                    q.push({courses[i][0],courses[i][1]});
-                }
-                else
-                {
-                    duration= duration-courses[i][0];
-                }
+                time+=courses[i][0];
+                valid.push_back(courses[i][0]);
             }
-            else
+            else //not a valid entry
             {
-                q.push({courses[i][0],courses[i][1]});
-                ans++;    
+                
+              //find the maxi in the valid list
+                int max_i=0;
+                for(int j=1;j<valid.size();j++)
+                {
+                    if(valid[j]> valid[max_i])
+                        max_i=j;    
+                }
+                
+                if(valid.size()>0 && valid[max_i] >courses[i][0])
+                {
+                   // cout<<i<<"else"<<endl;
+                    time=time-valid[max_i]+courses[i][0];
+                    valid[max_i]=courses[i][0];
+                    ///cout<<"time"<<va<<endl;
+                }
             }
         }
         
-        return q.size();;
+        // for(auto x: valid)
+        //     cout<<x<<endl;
+        return valid.size();
     }
 };

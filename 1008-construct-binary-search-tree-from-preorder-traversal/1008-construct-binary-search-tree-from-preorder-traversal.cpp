2d383e34fa48be8aea4ed85e9342bfba224e8836
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-25 16:48:47 +0530

    Time: 10 ms (39.37%), Space: 13.8 MB (47.42%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,45 +11,51 @@
  */
 class Solution {
 public:
-//     void makeTree(TreeNode* node, int i)
+    
+    //method-1 TC: o(N^2)
+//     TreeNode* makeTree(TreeNode* node, int i)
 //     {
 //         if(node==NULL)
 //         {
 //             node=new TreeNode(i);
-//             return;
+//             return node;
 //         }
 //         if(i>node->val)
-//             makeTree(node->right,i);
+//            node->right= makeTree(node->right,i);
 //         else
-//             makeTree(node->left,i);
+//            node->left= makeTree(node->left,i);
         
-//         return;
+//         return node;
 //     }
     
-    TreeNode* makeTree(TreeNode* node, int i)
-    {
-        if(node==NULL)
-        {
-            node=new TreeNode(i);
-            return node;
-        }
-        if(i>node->val)
-           node->right= makeTree(node->right,i);
-        else
-           node->left= makeTree(node->left,i);
+//     TreeNode* bstFromPreorder(vector<int>& preorder) {
         
-        return node;
-    }
+//         TreeNode* root=NULL;
+//         for(auto x:preorder)
+//             root= makeTree(root,x);
+        
+//         return root;
+//     }
     
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
+    // method-2 bst using preorder and inorder TC: Nlog(N)=>sorting the vector
+    
+    //method-3
+    TreeNode* solve(int &i, vector<int>& preorder, int bound)
+    {
+        if(i>=preorder.size() || preorder[i]>=bound) return NULL;
         
-        TreeNode* root=NULL;
-        for(auto x:preorder)
-        {
-            TreeNode* node=root;
-            root= makeTree(node,x);
-        }
+        TreeNode* root= new TreeNode(preorder[i]);
+        i++;
+        
+        root->left=solve(i,preorder,root->val);
+        root->right=solve(i,preorder,bound);
         
         return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder){
+        
+        int i=0;
+        return solve(i,preorder,INT_MAX);
+        
     }
 };

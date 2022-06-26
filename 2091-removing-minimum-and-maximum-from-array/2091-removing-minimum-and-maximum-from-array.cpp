da419b0734246a4517cc81c710d805cef20e017d
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-26 14:50:52 +0530

    Time: 278 ms (31.48%), Space: 88.3 MB (34.39%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -24,23 +24,23 @@ public:
         int p1=max(a,b)+1;
         int p2=n-(min(a,b));
         
-        int a_flag=1,b_flag=1;
-        if(a>=n/2)
-        {
-            a=n-a-1;
-            a_flag=0;
-        }
+//         int a_flag=1,b_flag=1;
+//         if(a>=n/2)
+//         {
+//             a=n-a-1;
+//             a_flag=0;
+//         }
         
-        if(b>=n/2)
-        {
-            b=n-b-1;
-            b_flag=0;
-        }
-        int p3=a+b+2;
+//         if(b>=n/2)
+//         {
+//             b=n-b-1;
+//             b_flag=0;
+//         }
+        //int p3=a+b+2;
+        
+        int p3= min(a,b)+1 + n-max(a,b);
             
         return min(p3,min(p1,p2));
-//         if(a_flag!=b_flag) return a+b+2;
-//         else
-//         return max(a,b)+1;
+
     }
 };

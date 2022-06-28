182bab70c5cb6b9b8efea76b428cc6eb6a2dc6c7
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-28 15:57:47 +0530

    Time: 308 ms (35.75%), Space: 121.2 MB (69.93%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -8,27 +8,35 @@ public:
         if(n==1) return 1;
         
         int l=0,r=1;
+       
+        //method-1
+        // while(r<n)
+        // {
+        //   if(nums[l]==nums[r] && l%2==flag)
+        //   {
+        //       int temp=0;
+        //       while(r<n && nums[l]==nums[r])
+        //       {
+        //           temp++;
+        //           r++;
+        //       }
+        //       del+=temp;
+        //       if(temp%2==1) flag=1-flag;
+        //       l=r;
+        //       r=r+1;
+        //   }
+        //  else
+        //  {
+        //     l++;
+        //     r++;
+        //  }
+        // }
         
-        while(r<n)
+        //method-2
+        for(int i=0;i<n-1;i++)
         {
-          if(nums[l]==nums[r] && l%2==flag)
-          {
-              int temp=0;
-              while(r<n && nums[l]==nums[r])
-              {
-                  temp++;
-                  r++;
-              }
-              del+=temp;
-              if(temp%2==1) flag=1-flag;
-              l=r;
-              r=r+1;
-          }
-         else
-         {
-            l++;
-            r++;
-         }
+            if( (i-del)%2==0 && nums[i]==nums[i+1])
+                del++;
         }
         
         if((n-del)%2==1) del++;

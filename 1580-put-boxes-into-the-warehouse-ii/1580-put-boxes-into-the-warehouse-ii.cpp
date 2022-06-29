5c391a458e69bc91ef06c9896a97e18735cddac1
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-29 17:19:44 +0530

    Time: 346 ms (28.00%), Space: 61.8 MB (38.00%) - LeetHub

---
+++ 1580-put-boxes-into-the-warehouse-ii/1580-put-boxes-into-the-warehouse-ii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int maxBoxesInWarehouse(vector<int>& b, vector<int>& w) {
+        
+        //make a vector consisting of largest size of box that can be pushed there
+        vector<int> v(w.size());
+        
+        //from left side
+        int temp=INT_MAX;
+        
+        for(int i=0;i<w.size();i++)
+        {
+            v[i]=temp;
+            if(w[i]<temp)
+                temp=w[i];
+        }
+        
+       
+        
+        //from right side
+        temp=INT_MAX;
+        for(int i=w.size()-1;i>=0;i--)
+        {
+            v[i]=min(w[i], max(v[i],temp));
+            
+            if(w[i]<temp)
+                temp=w[i];
+        }
+        
+         
+        
+        sort(b.begin(),b.end(),greater<int>());
+        sort(v.begin(), v.end(),greater<int>());
+        
+        // for(auto x:v)
+        //     cout<<x<<endl;
+        
+        int cnt=0;
+        int i=0,j=0;
+        
+        while(i<b.size() && j<v.size())
+        {
+            //cout<<b[i]<<" "<<v[j]<<endl;
+            if(b[i]<=v[j])
+            {
+                i++;
+                j++;
+                cnt++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+        return cnt;
+        
+    }
+};

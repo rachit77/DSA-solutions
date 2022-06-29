2e6a7aa7ac747ab84f0e21ddee51180327a3884e
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-06-29 16:41:51 +0530

    Time: 213 ms (58.90%), Space: 60.4 MB (83.56%) - LeetHub

--- 1564-put-boxes-into-the-warehouse-i/1564-put-boxes-into-the-warehouse-i.cpp
+++ 1564-put-boxes-into-the-warehouse-i/1564-put-boxes-into-the-warehouse-i.cpp
@@ -2,33 +2,51 @@ class Solution {
 public:
     int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& w) {
         
+        //method-1
         //change the warehouse size
-        int temp=INT_MAX;
-        for(int i=0;i<w.size();i++)
-        {
-           w[i]=min(w[i],temp);
+//         int temp=INT_MAX;
+//         for(int i=0;i<w.size();i++)
+//         {
+//            w[i]=min(w[i],temp);
             
-            if(w[i]<temp)
-                temp=w[i];
-        }
+//             if(w[i]<temp)
+//                 temp=w[i];
+//         }
         
+//         sort(boxes.begin(),boxes.end());
+        
+//         int cnt=0,i=w.size()-1,j=0;
+        
+//         while(i>=0 && j<boxes.size())
+//         {
+//            if(boxes[j]<=w[i]) // can fit the box here
+//            {
+//                i--;
+//                j++;
+//                cnt++;
+//            }
+//             else
+//             {
+//                 i--; // check pev warehouse
+//             }
+//         }
+//         return cnt;
+        
+        //method-2 using o(1) space
+        int cnt=0;
+        int i=boxes.size()-1;
         sort(boxes.begin(),boxes.end());
         
-        int cnt=0,i=w.size()-1,j=0;
-        
-        while(i>=0 && j<boxes.size())
+        for(int room: w)
         {
-           if(boxes[j]<=w[i]) // can fit the box here
-           {
-               i--;
-               j++;
-               cnt++;
-           }
-            else
-            {
-                i--; // check pev warehouse
-            }
+            while(i>=0 && boxes[i]>room)
+                i--;
+            
+            if(i==-1) return cnt;
+            cnt++;
+            i--;
         }
+        
         return cnt;
     }
 };

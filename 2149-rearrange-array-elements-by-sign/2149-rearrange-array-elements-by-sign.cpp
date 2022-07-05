34e71d568eb1e60b319f0b70081f069342313ba0
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-05 15:09:20 +0530

    Time: 329 ms (46.01%), Space: 123.6 MB (99.60%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,32 +1,57 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        
+        //method-1
+//         int n=nums.size();
+//         vector<int> ans,v1,v2;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//           if(nums[i]>0)
+//               v1.push_back(nums[i]);
+//             else
+//                 v2.push_back(nums[i]);
+//         }
+        
+//         int a=0,b=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(i%2==0)
+//             {
+//                ans.push_back(v1[a]) ;
+//                 a++;
+//             }
+//             else
+//             {
+//                 ans.push_back(v2[b]) ;
+//                 b++;
+//             }
+//         }
+        
+//         return ans;
+        
+        //method-2
         int n=nums.size();
-        vector<int> ans,v1,v2;
+        vector<int> ans(n);
+        
+        int pos=0,neg=1;
         
         for(int i=0;i<n;i++)
         {
-          if(nums[i]>0)
-              v1.push_back(nums[i]);
-            else
-                v2.push_back(nums[i]);
-        }
-        
-        int a=0,b=0;
-        for(int i=0;i<n;i++)
-        {
-            if(i%2==0)
+            if(nums[i]>0)
             {
-               ans.push_back(v1[a]) ;
-                a++;
+                ans[pos]=nums[i];
+                pos+=2;
             }
             else
             {
-                ans.push_back(v2[b]) ;
-                b++;
+                ans[neg]=nums[i];
+                neg+=2;
             }
         }
         
         return ans;
+        
     }
 };

a7d4e007f0494208894dc5ce0afebb7514cb28e2
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-05 13:02:10 +0530

    Time: 129 ms (83.17%), Space: 33.8 MB (85.63%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s;
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
-            s.insert(nums[i]);
+//         unordered_set<int> s;
+//         int ans=0;
+//         for(int i=0;i<nums.size();i++)
+//             s.insert(nums[i]);
         
-        for(int i=0;i<nums.size();i++)
-        {
-            if(s.find(nums[i]-1) == s.end()) //nums[i] is smallest //of that seq
-            {
-                int j= nums[i]+1,count=1;
-                    while(s.find(j)!=s.end())
-                    {
-                        j++;
-                        count++;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(s.find(nums[i]-1) == s.end()) //nums[i] is smallest //of that seq
+//             {
+//                 int j= nums[i]+1,count=1;
+//                     while(s.find(j)!=s.end())
+//                     {
+//                         j++;
+//                         count++;
                         
-                    }
-                ans =max(ans,count);
+//                     }
+//                 ans =max(ans,count);
+//             }
+//         }
+//        return ans;       
+        
+//     }
+        
+     //method-2 sorting
+        int n=nums.size();
+        if(n==0) return 0;
+        sort(nums.begin(),nums.end());
+        int max_streak=1;
+        int cur_streak=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i-1]==nums[i]) continue;
+             if(nums[i-1]+1==nums[i])
+             {
+                 cur_streak+=1;
+             }
+            else
+            {
+                max_streak=max(cur_streak,max_streak);
+                cur_streak=1;
             }
         }
-       return ans;       
         
+        return max(cur_streak,max_streak);
     }
 };

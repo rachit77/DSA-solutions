8e1b78590e55c9e2ba2f0510e68ee0fb6a65aee0
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-07 02:17:35 +0530

    Time: 5 ms (65.30%), Space: 6.3 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -40,37 +40,63 @@ public:
         // return find_interleave(s1,i,s2,j,res,s3,dp);
         
         //method-2 tabulation sc:O(s1.len * s2.len)
+//         int s1_len=s1.length(), s2_len=s2.length();
+//         //make a dp
+//         bool dp[s1_len+1][s2_len+1];
         
+//         dp[0][0]=true;
+        
+//         //fill the first row
+//         for(int j=1;j<s2_len+1;j++)
+//         {
+//             // bool temp= (s2[j-1]==s3[j-1]);
+//             dp[0][j]= (dp[0][j-1] && (s2[j-1]==s3[j-1]));
+//         }
+        
+//         //fill the first column
+//         for(int i=1;i<s1_len+1;i++)
+//         {
+//            bool temp= (s1[i-1]==s3[i-1]);
+//             dp[i][0]=dp[i-1][0] && temp;
+//         }
+    
+//         for(int i=1;i<s1_len+1;i++)
+//         {
+//             for(int j=1;j<s2_len+1;j++)
+//             {
+                
+//                 dp[i][j]=(dp[i-1][j] && (s1[i-1]==s3[i+j-1]))||(dp[i][j-1] && (s2[j-1]==s3[i+j-1]));
+//             }
+//         }
+        
+//         return dp[s1_len][s2_len];
+        
+        
+         //method-3 tabulation sc:O(s1.len or s2.len)
         int s1_len=s1.length(), s2_len=s2.length();
         //make a dp
-        bool dp[s1_len+1][s2_len+1];
+        bool dp[s2_len+1];
+        dp[0]=true;
         
-        dp[0][0]=true;
-        
-        //fill the first row
-        for(int j=1;j<s2_len+1;j++)
+        for(int i=0;i<=s1_len;i++)   //s1 string
         {
-            // bool temp= (s2[j-1]==s3[j-1]);
-            dp[0][j]= (dp[0][j-1] && (s2[j-1]==s3[j-1]));
-        }
-        
-        //fill the first column
-        for(int i=1;i<s1_len+1;i++)
-        {
-           bool temp= (s1[i-1]==s3[i-1]);
-            dp[i][0]=dp[i-1][0] && temp;
-        }
-    
-        for(int i=1;i<s1_len+1;i++)
-        {
-            for(int j=1;j<s2_len+1;j++)
+            for(int j=0;j<=s2_len;j++)  // s2 string
             {
+                bool t1=false,t2=false;
+                if(i!=0)
+                {
+                   t1= (s1[i-1]==s3[i+j-1] && dp[j]); 
+                }
+                if(j!=0)
+                {
+                    t2= (s2[j-1]==s3[i+j-1] && dp[j-1]);
+                }
                 
-                dp[i][j]=(dp[i-1][j] && (s1[i-1]==s3[i+j-1]))||(dp[i][j-1] && (s2[j-1]==s3[i+j-1]));
+                if(!(i==0 && j==0))
+                dp[j]= t1 || t2;
             }
         }
         
-        return dp[s1_len][s2_len];
-        
+        return dp[s2_len];
     }
 };

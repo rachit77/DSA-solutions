2fa95b893042d53e3437b9ecfb6880669bdd448b
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-06 11:18:29 +0530

    Time: 19 ms (22.24%), Space: 6 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int find(int n)
+    {
+        if(n==0) return 0;
+        if(n==1)  return 1;
+        return find(n-1)+ find(n-2);
+    }
+    int fib(int n) {
+        
+        return find(n);
+    }
+};

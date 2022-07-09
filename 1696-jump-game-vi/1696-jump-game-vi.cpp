2b8ea05458eef03b4369f9d37d63973c40fe4bf1
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-09 13:30:17 +0530

    Time: 262 ms (66.92%), Space: 94 MB (33.65%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    
+    //method-2 mm=emo TLE
+//     int find(int st,int &k,vector<int>& nums,vector<int> &dp)
+//     {
+//         if(st==nums.size()-1) return nums[nums.size()-1];
+        
+//         if(st>=nums.size()) return INT_MIN;
+        
+//         if(dp[st]!=-1) return dp[st];
+        
+//         int max_sum=INT_MIN;
+//         for(int i=st+1;i<=st+k;i++)
+//         {
+//             if(i>nums.size()) break;
+//             int temp=find(i,k,nums,dp);
+//             max_sum=max(max_sum,temp);
+//         }
+        
+//         return dp[st]=(max_sum==INT_MIN)?max_sum:max_sum+nums[st];
+//     }
+    
+    
+    int maxResult(vector<int>& nums, int k) {
+       
+        int n=nums.size();
+        
+        //method-1  top dowm with max heap nlogn
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        priority_queue<pair<int,int>, vector<pair<int,int>> > pq;
+        pq.push({nums[0],0});
+        
+        for(int i=1;i<n;i++)
+        {
+            int temp;
+            while(!pq.empty() && pq.top().second<(i-k))
+            {
+                pq.pop();
+            }
+            
+            temp=pq.top().first;
+            dp[i]=nums[i]+temp;
+            
+            pq.push({dp[i],i});
+        }
+        return dp[n-1];
+        
+        // //Method-2 recursion and memoization
+        // vector<int> dp(n,-1);
+        // return find(0,k,nums,dp);
+        
+        
+        //method-3
+    }
+};

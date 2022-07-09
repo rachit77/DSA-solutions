20110e68734de7b85db384e7fdcd63b3c40f42d2
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-09 16:07:17 +0530

    Time: 290 ms (57.39%), Space: 298.1 MB (5.05%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -27,31 +27,50 @@ public:
         int n=nums.size();
         
         //method-1  top dowm with max heap nlogn
-        vector<int> dp(n,0);
-        dp[0]=nums[0];
-        priority_queue<pair<int,int>, vector<pair<int,int>> > pq;
-        pq.push({nums[0],0});
+//         vector<int> dp(n,0);
+//         dp[0]=nums[0];
+//         priority_queue<pair<int,int>, vector<pair<int,int>> > pq;
+//         pq.push({nums[0],0});
         
-        for(int i=1;i<n;i++)
-        {
-            int temp;
-            while(!pq.empty() && pq.top().second<(i-k))
-            {
-                pq.pop();
-            }
+//         for(int i=1;i<n;i++)
+//         {
+//             int temp;
+//             while(!pq.empty() && pq.top().second<(i-k))
+//             {
+//                 pq.pop();
+//             }
             
-            temp=pq.top().first;
-            dp[i]=nums[i]+temp;
+//             temp=pq.top().first;
+//             dp[i]=nums[i]+temp;
             
-            pq.push({dp[i],i});
-        }
-        return dp[n-1];
+//             pq.push({dp[i],i});
+//         }
+//         return dp[n-1];
         
-        // //Method-2 recursion and memoization
+        // //Method-2 recursion and memoization TLE
         // vector<int> dp(n,-1);
         // return find(0,k,nums,dp);
         
         
-        //method-3
+        //method-3 DP + deque
+         vector<int> dp(n);
+          dp[0]=nums[0];
+        deque<int> dq; //stores the index
+        dq.push_back(0);
+        
+        for(int i=1;i<n;i++)
+        {
+            //pop the invalid indices
+            while(!dq.empty() && dq.back()<i-k) dq.pop_back();
+            
+            dp[i]=nums[i]+ dp[dq.back()];
+            
+            //pop the smaller values than dp[i]
+            while(!dq.empty() && dp[i]>=dp[dq.front()])
+                dq.pop_front();
+            
+            dq.push_front(i);
+        }
+        return dp[n-1];
     }
 };

c394ace5f39d3c4bb8aa300917ff35eb30408957
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-09 23:36:36 +0530

    Time: 7 ms (19.73%), Space: 5.9 MB (33.50%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(int n) {
+       
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=(2*i)+1;
+        }
+        
+        sum=sum/n;
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int num=2*i +1;
+            if(num<sum)
+                ans+=(sum-num);
+        }
+        
+        return ans;
+    }
+};
+

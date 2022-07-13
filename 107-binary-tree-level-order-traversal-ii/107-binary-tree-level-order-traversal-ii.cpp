fd57de3f2b4c1c98c249c1d608e6a614a962b09d
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-13 12:03:08 +0530

    Time: 7 ms (56.96%), Space: 12.3 MB (99.37%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
@@ -11,37 +11,72 @@
  */
 class Solution {
 public:
+    int find_height(TreeNode* root)
+    {
+        if(root==NULL) return 0;
+        int l=0,r=0;
+        
+        l=find_height(root->left);
+        r=find_height(root->right);
+        
+        return 1+max(l,r);
+    }
+    
+    void level_order(TreeNode* root, vector<vector<int>> &ans, int &h, int cur_h)
+    {
+        if(root==NULL) return;
+        
+        ans[h-cur_h-1].push_back(root->val);
+        
+        //left side
+        level_order(root->left,ans,h,cur_h+1);
+        //right side
+        level_order(root->right,ans,h,cur_h+1);
+        
+        return;
+    }
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+ 
         
-        vector<vector<int>> ans;
-        if(!root) return ans;
+        //method-1
+//         vector<vector<int>> ans;
+//         if(!root) return ans;
         
-        queue<TreeNode*> q;
-        q.push(root);
+//         queue<TreeNode*> q;
+//         q.push(root);
         
-        stack<vector<int>> st;
+//         stack<vector<int>> st;
         
-        while(!q.empty())
-        {
-            int siz=q.size();
-            vector<int> temp;
-            for(int i=0;i<siz;i++)
-            {
-                TreeNode* node=q.front();
-                q.pop();
-                temp.push_back(node->val);
+//         while(!q.empty())
+//         {
+//             int siz=q.size();
+//             vector<int> temp;
+//             for(int i=0;i<siz;i++)
+//             {
+//                 TreeNode* node=q.front();
+//                 q.pop();
+//                 temp.push_back(node->val);
                 
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            st.push(temp);
-        }
+//                 if(node->left) q.push(node->left);
+//                 if(node->right) q.push(node->right);
+//             }
+//             st.push(temp);
+//         }
         
-        while(!st.empty())
-        {
-            ans.push_back(st.top());
-            st.pop();
-        }
+//         while(!st.empty())
+//         {
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+        
+//         return ans;
+        
+        
+        //method-2
+        int h=find_height(root);
+        vector<vector<int>> ans(h);
+        int cur_h=0;
+        level_order(root,ans,h,cur_h);
         
         return ans;
     }

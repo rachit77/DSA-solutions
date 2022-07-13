035e321e3bf6c688a294fe20a4ab896be3fc38b7
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-13 14:33:15 +0530

    Time: 6 ms (46.83%), Space: 11.1 MB (50.63%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) { 
+        
+        //check if both of them are on same level
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        int flag=0,par=0;
+        
+        
+        
+        while(!q.empty())
+        {
+            int cur_size=q.size();
+            
+            for(int i=0;i<cur_size;i++)
+            {
+                TreeNode* curr=q.front();
+                q.pop();
+                
+                if(curr->val==x || curr->val==y) flag++;
+                
+                int l=0,r=0;
+                if(curr->left)
+                {
+                    if(curr->left->val==x || curr->left->val==y) l=1;
+                    q.push(curr->left);
+                }
+                
+                if(curr->right)
+                {
+                    if(curr->right->val==x || curr->right->val==y) r=1;
+                    q.push(curr->right);
+                }
+                
+                if(l==1 && r==1)
+                {
+                    par=1;
+                    break;
+                }
+            }
+            
+            if(flag!=0) break;
+        }
+        
+        //if(par==1 || falg==1) return false;
+        
+        if(par==0 && flag==2) return true;
+        
+        else return false;
+        
+    }
+};

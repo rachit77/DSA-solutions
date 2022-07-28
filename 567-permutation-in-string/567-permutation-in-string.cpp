d5ffc3d5fc62ba36c34d729c1475ba4e7d3939ea
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-28 18:26:26 +0530

    Time: 9 ms (76.05%), Space: 7.4 MB (33.83%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool check(vector<int> &p,string &s, int st, int &m)
+    {
+        vector<int> temp(26,0);
+        
+        for(int i=0;i<m;i++)
+        {
+            temp[s[i+st]-'a']++;
+        }
+        
+        for(int i=0;i<26;i++)
+        {
+            if(p[i]!=temp[i]) return false;
+        }
+        
+        return true;
+    }
+    bool checkInclusion(string p, string s) {
+        int n=s.length(),m=p.length();
+        
+        
+        if(n<m) return false;
+        int cur_hash=0,p_hash=0;
+        
+        vector<int> pcount(26,0);
+        //cal starting hash
+        for(int i=0;i<m;i++)
+        {
+            pcount[p[i]-'a']++;
+            cur_hash+=(s[i]-'a');
+            p_hash+= (p[i]-'a');
+        }
+        
+        if(cur_hash==p_hash)
+        {
+            if(check(pcount,s,0,m)) return true;
+        }
+            
+        
+        for(int i=m;i<n;i++)
+        {
+            cur_hash= cur_hash + (s[i]-'a') -(s[i-m]-'a');
+            if(cur_hash==p_hash)
+            {
+                if(check(pcount,s,i-m+1,m)) return true;
+            }
+        }
+        
+        return false;
+    }
+};

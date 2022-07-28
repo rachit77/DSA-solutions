e6626e3a0777a9f1d551c3d751addc151445987c
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2022-07-28 10:09:37 +0530

    Time: 7 ms (87.80%), Space: 7.3 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        vector<int> v(26,0);
+        
+        for(auto it:s)
+            v[it-'a']++;
+        
+        for(auto it: t)
+            v[it-'a']--;
+        
+        for(int i=0;i<26;i++)
+        {
+            if(v[i]!=0)
+                return false;
+        }
+        
+        return true;
+    }
+};
